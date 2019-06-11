02893ec3636c342cc6b8161129076d686a9ee5b0
Kieran O'Grady <42932573+kieranogrady@users.noreply.github.com>
2019-06-11 20:51:00 +0100

    Add StringReverse features with test coverage

--- test/TestStringReverse.cpp
+++ test/TestStringReverse.cpp
@@ -11,5 +11,20 @@ TEST_CASE("A default constructed StringReverse is empty", "[stringreverse]") {
 
 TEST_CASE("A StringReverse constructed with 'Hello World' becomes 'dlroW olleH'", "[stringreverse]") {
     const StringReverse s{"Hello World"};
-    //TODO: test it
+    CHECK_FALSE(s == "Hello World");
+    CHECK_FALSE(s.empty());
+    CHECK(s == "dlroW olleH");
+}
+
+TEST_CASE("StringReverse returns reversed string", "[stringreverse]") {
+    const StringReverse s{"LondonUni"};
+    std::string out = s;
+    CHECK(out == "inUnodnoL");
+}
+
+TEST_CASE("Move from a StringReverse to our own string", "[stringreverse]") {
+    StringReverse s{"LondonUni"};
+    std::string out = std::move(s);
+    CHECK(out == "inUnodnoL");
+    CHECK(s.empty());
 }
